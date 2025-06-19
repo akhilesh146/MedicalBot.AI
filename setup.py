23cade067c2579c0ba9ead33283b3c328dff0ca3
akhilesh146 <akhileshsamarth146@gmail.com>
2025-06-19 07:59:41 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Akhilesh Samarth',
+    author_email='akhileshsamarth9@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
